db809428ed3c885b7dfd43245d410566616e3add
Markus Reynolds <markusreynolds1989@gmail.com>
2023-04-08 09:28:15 -0500

    Started work on ML instruction.

---
+++ MachineLearning/Program.fs
@@ -0,0 +1,18 @@
+﻿namespace MachineLearning
+
+open System
+open System.IO
+open Microsoft.ML
+open Microsoft.ML.Data
+
+[<CLIMutable>]
+type Data =
+    {
+        mutable Label: string
+    }
+    
+module Main =
+    [<EntryPoint>]
+    let main args =
+        
+        0

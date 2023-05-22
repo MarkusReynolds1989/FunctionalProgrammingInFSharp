440a96f913f0111cf2871c29d7efe3d404970653
Markus Reynolds <markusreynolds1989@gmail.com>
2023-05-21 19:35:00 -0500

    Fixed machine learning.

--- src/MachineLearning/Program.fs
+++ src/MachineLearning/Program.fs
@@ -1,62 +1,69 @@
-﻿namespace MachineLearning
-
-open Microsoft.ML
-open Microsoft.ML.Trainers
-open Microsoft.ML.Data
-
-module Main =
-    [<CLIMutable>]
-    type Game =
-        { ProductID: int
-          Price: decimal
-          Name: string }
+﻿module Main =
+    open Microsoft.ML
+    open Microsoft.ML.Data
+    open System
+    open System.IO
+    open Microsoft.ML.Trainers
 
     [<CLIMutable>]
-    type Transaction =
-        { [<LoadColumn(0); KeyType(count = 11UL)>]
-          TransactionID: uint32
-          [<LoadColumn(1); KeyType(count = 11UL)>]
-          ProductID: uint32 }
+    type ProductEntry =
+        { [<LoadColumn(0); KeyType(count = 45UL)>]
+          ProductID: uint32
+          [<LoadColumn(1); KeyType(count = 45UL)>]
+          OtherProductID: uint32
+          [<NoColumn>]
+          Label: float32 }
 
     [<CLIMutable>]
     type Prediction = { Score: float32 }
 
-    [<EntryPoint>]
-    let main args =
+    let assemblyFolderPath =
+        Path.GetDirectoryName(Reflection.Assembly.GetExecutingAssembly().Location)
 
-        let path =
-            @"C:\Users\marku\home\code\F#\FunctionalProgrammingInFSharp\src\MachineLearning\transaction_list.tsv"
+    let trainPath =
+        "C:/Users/marku/home/code/f#/FunctionalProgrammingInFSharp/src/MachineLearning/transaction_list.tsv"
 
-        let mlContext = MLContext()
+    let mlContext = MLContext()
 
-        let trainData =
-            let columns =
-                [| TextLoader.Column("TransactionID", DataKind.UInt32, source = [| TextLoader.Range(0) |])
-                   TextLoader.Column("ProductID", DataKind.UInt32, source = [| TextLoader.Range(1) |]) |]
+    let trainData =
+        let columns =
+            [| TextLoader.Column("Label", DataKind.Single, 0)
+               TextLoader.Column(
+                   "ProductID",
+                   DataKind.UInt32,
+                   source = [| TextLoader.Range(0) |],
+                   keyCount = KeyCount 45UL
+               )
+               TextLoader.Column(
+                   "OtherProductID",
+                   DataKind.UInt32,
+                   source = [| TextLoader.Range(1) |],
+                   keyCount = KeyCount 45UL
+               ) |]
 
-            mlContext.Data.LoadFromTextFile(path, columns, hasHeader = true, separatorChar = '\t')
+        mlContext.Data.LoadFromTextFile(trainPath, columns, hasHeader = true, separatorChar = '\t')
 
-        let options =
-            MatrixFactorizationTrainer.Options(
-                MatrixColumnIndexColumnName = "TransactionID",
-                MatrixRowIndexColumnName = "ProductID",
-                LossFunction = MatrixFactorizationTrainer.LossFunctionType.SquareLossOneClass,
-                Alpha = 0.01,
-                Lambda = 0.025
-            )
+    let options =
+        MatrixFactorizationTrainer.Options(
+            MatrixColumnIndexColumnName = "ProductID",
+            MatrixRowIndexColumnName = "OtherProductID",
+            LossFunction = MatrixFactorizationTrainer.LossFunctionType.SquareLossOneClass,
+            LabelColumnName = "Label",
+            Alpha = 0.01,
+            Lambda = 0.025
+        )
 
-        let est = mlContext.Recommendation().Trainers.MatrixFactorization(options)
+    let est = mlContext.Recommendation().Trainers.MatrixFactorization(options)
 
-        let model = est.Fit(trainData)
+    let model = est.Fit(trainData)
 
-        let predictionEngine =
-            mlContext.Model.CreatePredictionEngine<Transaction, Prediction>(model)
+    let predictionEngine =
+        mlContext.Model.CreatePredictionEngine<ProductEntry, Prediction>(model)
 
-        let prediction =
-            predictionEngine.Predict
-                { TransactionID = 3u
-                  ProductID = 23u}
+    let prediction =
+        predictionEngine.Predict
+            { ProductID = 1u
+              OtherProductID = 15u
+              Label = 0.f }
 
-        printfn "%f" prediction.Score
-
-        0
+    printfn $"ProductID = 1 and OtherProductID = 15\nPredicted Score: %f{prediction.Score}"
